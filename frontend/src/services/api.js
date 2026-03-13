8bd506c65f98a516ef0d3517a54a3209c7335805
Khalid Latif <khalid.latif@ibm.com>
2026-03-13 22:25:48 +0530

    test deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,13 +2,13 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 export async function sendMessage(message){
 
-const res = await fetch(`${API_URL}/chat`,{
- method:"POST",
- headers:{
-  "Content-Type":"application/json"
- },
- body:JSON.stringify({message}) ,
-});
+ const res = await fetch("/chat",{
+  method:"POST",
+  headers:{
+   "Content-Type":"application/json"
+  },
+  body:JSON.stringify({message})
+ });
 
-return res.json();
+ return res.json();
 }
